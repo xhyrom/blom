cba6475cef464289c683e809da8890e8b2eb04df
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-12-23 17:23:50 +0100

    feat: transpiler abstraction

--- transpiler/transpiler.go
+++ transpiler/transpiler.go
@@ -4,4 +4,13 @@ import "blom/ast"
 
 type Transpiler interface {
 	Transpile(program *ast.Program) (string, error)
+	TranspileBlock(block ast.BlockStatement) string
+	TranspileStatement(stmt ast.Statement) string
+	TranspileFunctionDeclaration(declaration *ast.FunctionDeclaration) string
+	TranspileBinaryExpression(expression *ast.BinaryExpression) string
+	TranspileUnaryExpression(expression *ast.UnaryExpression) string
+	TranspileDeclarationStatement(declaration *ast.DeclarationStatement) string
+	TranspileReturnStatement(statement *ast.ReturnStatement) string
+	TranspileIfStatement(statement *ast.IfStatement) string
+	TranspileFunctionCall(call *ast.FunctionCall) string
 }
