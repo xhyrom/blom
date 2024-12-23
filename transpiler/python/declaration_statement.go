911cba1c72431ca562f637832896c0347f0ea0b1
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-12-23 22:43:19 +0100

    fix(python): scoping

--- transpiler/python/declaration_statement.go
+++ transpiler/python/declaration_statement.go
@@ -6,26 +6,34 @@ import (
 	"fmt"
 )
 
-// TODO: fix for more nested environments
 func (t PythonTranspiler) TranspileDeclarationStatement(declaration *ast.DeclarationStatement, environment *env.Environment, indent int) string {
 	skip := false
 
 	if environment.Parent != nil && environment.Parent.FindVariable(declaration.Name) != nil {
 		if !declaration.Redeclaration && environment.Get(declaration.Name) == nil {
-			environment.Set(declaration.Name, &env.BooleanObject{})
+			environment.Set(declaration.Name, &env.IntegerObject{
+				Value: int64(indent),
+			})
 
 			return fmt.Sprintf("%s_%d = %s\n", declaration.Name, indent, t.TranspileAndFunctionifyStatement(declaration.Value, environment, indent))
 		}
 	} else {
 		skip = true
 
-		environment.Set(declaration.Name, &env.BooleanObject{
-			Value: false,
+		environment.Set(declaration.Name, &env.IntegerObject{
+			Value: int64(indent),
 		})
 	}
 
 	if environment.Get(declaration.Name) != nil && !skip {
 		return fmt.Sprintf("%s_%d = %s\n", declaration.Name, indent, t.TranspileAndFunctionifyStatement(declaration.Value, environment, indent))
+	}
+
+	if environment.Parent.Get(declaration.Name) != nil && !skip {
+		id := environment.Parent.Get(declaration.Name).(*env.IntegerObject).Value
+		if id > 1 {
+			return fmt.Sprintf("%s_%d = %s\n", declaration.Name, id, t.TranspileAndFunctionifyStatement(declaration.Value, environment, indent))
+		}
 	}
 
 	return fmt.Sprintf("%s = %s\n", declaration.Name, t.TranspileAndFunctionifyStatement(declaration.Value, environment, indent))
