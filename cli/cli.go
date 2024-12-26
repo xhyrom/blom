fa2f39b649a6064e14f721d7eed35b56c04a8e28
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-12-26 19:58:38 +0100

    feat: cli

---
+++ cli/cli.go
@@ -0,0 +1,63 @@
+package cli
+
+import (
+	"blom/interpreter"
+	"blom/lexer"
+	"blom/parser"
+	"blom/tokens"
+	"os"
+	"strings"
+
+	"github.com/gookit/goutil/dump"
+)
+
+func Run(args []string) {
+	var emitTokens bool
+	var emitAst bool
+	var inputFile string
+
+	for _, arg := range args {
+		switch arg {
+		case "-tokens", "--emit-tokens":
+			emitTokens = true
+		case "-ast", "--emit-ast":
+			emitAst = true
+		default:
+			if strings.HasSuffix(arg, ".blom") {
+				inputFile = arg
+			}
+		}
+	}
+
+	if inputFile == "" {
+		panic("No input file specified")
+	}
+
+	content, err := os.ReadFile(inputFile)
+	if err != nil {
+		panic(err)
+	}
+
+	lex := lexer.New(inputFile, string(content))
+	tkns := make([]tokens.Token, 0)
+
+	current := lex.Next()
+	for current.Kind != tokens.Eof {
+		tkns = append(tkns, *current)
+		current = lex.Next()
+	}
+
+	if emitTokens {
+		dump.Println(tkns)
+	}
+
+	parser := parser.New(inputFile)
+	ast := parser.AST(inputFile, string(content))
+
+	if emitAst {
+		dump.Println(ast)
+	}
+
+	inp := interpreter.New()
+	dump.Println(inp.Interpret(ast))
+}
--- main.go
+++ main.go
@@ -1,71 +1,10 @@
 package main
 
 import (
-	"blom/interpreter"
-	"blom/lexer"
-	"blom/parser"
-	"blom/tokens"
-	"fmt"
+	"blom/cli"
 	"os"
-
-	"github.com/gookit/goutil/dump"
 )
 
 func main() {
-	args := os.Args[1:]
-
-	if len(args) == 0 {
-		fmt.Fprintf(os.Stderr, "Usage: %s <file>\n", os.Args[0])
-		os.Exit(1)
-	}
-
-	content, err := os.ReadFile(args[0])
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error reading file: %s\n", err)
-		os.Exit(1)
-	}
-
-	lexer := lexer.New(args[0], string(content))
-	tkns := make([]tokens.Token, 0)
-
-	c := lexer.Next()
-	for c.Kind != tokens.Eof {
-		tkns = append(tkns, *c)
-		c = lexer.Next()
-	}
-
-	dump.Println(tkns)
-
-	pp := parser.New(args[0])
-	ast := pp.AST(args[0], string(content))
-
-	dump.Config(func(o *dump.Options) {
-		o.MaxDepth = 10
-	})
-
-	dump.Println(ast)
-
-	inp := interpreter.New()
-	dump.Println(inp.Interpret(ast))
-
-	/*pp = parser.New(args[0])
-	ast = pp.AST(args[0], string(content))
-	transp := python.PythonTranspiler{}
-	code, _ := transp.Transpile(ast)
-
-	fmt.Printf("%s\n", code)
-
-	/*pp = parser.New(args[0])
-	ast = pp.AST(args[0], string(content))
-	transp := lua.LuaTranspiler{}
-	code, _ := transp.Transpile(ast)
-
-	fmt.Printf("%s\n", code)
-
-	pp = parser.New(args[0])
-	ast = pp.AST(args[0], string(content))
-	transpp := javascript.JavascriptTranspiler{}
-	code, _ = transpp.Transpile(ast)
-
-	fmt.Printf("%s\n", code)*/
+	cli.Run(os.Args)
 }
