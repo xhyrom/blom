92652cbc93c6da1d0e6ef4cd5e5cbc32cb981d3c
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2025-01-03 22:49:40 +0100

    refactor: cleanup

--- debug/debug.go
+++ debug/debug.go
@@ -67,6 +67,15 @@ func (s *SourceLocation) Stringify() string {
 }
 
 func (s *SourceLocation) ThrowError(msg string, top bool, hints ...*Hint) {
+	s.throw("\x1b[31;1mError", msg, top, hints...)
+	os.Exit(1)
+}
+
+func (s *SourceLocation) ThrowWarning(msg string, top bool, hints ...*Hint) {
+	s.throw("\x1b[33;1mWarning", msg, top, hints...)
+}
+
+func (s *SourceLocation) throw(prefix string, msg string, top bool, hints ...*Hint) {
 	if !top {
 		fmt.Fprintln(os.Stderr, "──────────────────────────────────────")
 	}
@@ -74,81 +83,10 @@ func (s *SourceLocation) ThrowError(msg string, top bool, hints ...*Hint) {
 	n, _ := fmt.Fprintf(os.Stderr, "[%s:%d:%d]", filepath.Base(s.File), s.Row, s.Column)
 
 	msgLines := strings.Split(msg, "\n")
-	fmt.Fprintf(os.Stderr, " \x1b[31;1mError\x1b[0m: %s\n", msgLines[0])
+	fmt.Fprintf(os.Stderr, " %s\x1b[0m: %s\n", prefix, msgLines[0])
 	for _, line := range msgLines[1:] {
 		fmt.Fprintf(os.Stderr, "%*s%s\n", n+8, "", line)
 	}
-
-	code, ok := fileCache[s.File]
-
-	if !ok {
-		code, _ = os.ReadFile(s.File)
-		fileCache[s.File] = code
-	}
-
-	lines := strings.Split(string(code), "\n")
-
-	fmt.Fprintln(os.Stderr, "━━━━━━━━━━━━━━━━ code ━━━━━━━━━━━━━━━━")
-
-	startLine := int64(s.Row - 5)
-	if startLine < 0 {
-		startLine = 0
-	}
-
-	width := len(fmt.Sprintf("%d", s.Row))
-
-	if len(hints) == 0 {
-		for i := uint64(startLine); i < s.Row; i++ {
-			fmt.Fprintf(os.Stderr, "%*d | %s\n", width, i+1, lines[i])
-		}
-
-		fmt.Fprintf(os.Stderr, "%*c |%*c", width, ' ', s.Column, ' ')
-		fmt.Fprintf(os.Stderr, "^\n")
-	} else {
-		for _, hint := range hints {
-			if hint != nil {
-				for i := uint64(startLine); i < s.Row-1; i++ {
-					fmt.Fprintf(os.Stderr, "%*d | %s\n", width, i+1, lines[i])
-				}
-
-				fmt.Fprintf(os.Stderr, "%*d | %s\x1b[32;1m%s\x1b[0m%s\n", width, s.Row, lines[s.Row-1][:s.Column-1], hint.Code, lines[s.Row-1][s.Column-1:])
-
-				fmt.Fprintf(os.Stderr, "%*c |%*c", width, ' ', s.Column, ' ')
-
-				if len(hint.Code) > 0 {
-					fmt.Fprintf(os.Stderr, "\x1b[32;1m%s\x1b[0m\n", strings.Repeat("+", len(hint.Code)))
-					fmt.Fprintf(os.Stderr, "%*c\x1b[34;1mHint\x1b[0m: %s\n", n, ' ', hint.Msg)
-
-				} else {
-					fmt.Fprintf(os.Stderr, "^")
-					fmt.Fprintf(os.Stderr, " \x1b[34;1mHint\x1b[0m: %s\n", hint.Msg)
-				}
-
-				if hint != hints[len(hints)-1] {
-					fmt.Fprintln(os.Stderr, "")
-				}
-			} else {
-				for i := uint64(startLine); i < s.Row; i++ {
-					fmt.Fprintf(os.Stderr, "%*d | %s\n", width, i+1, lines[i])
-				}
-				fmt.Fprintf(os.Stderr, "%*c |%*c", width, ' ', s.Column, ' ')
-				fmt.Fprintf(os.Stderr, "^\n")
-			}
-		}
-	}
-
-	fmt.Fprintln(os.Stderr, "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
-
-	os.Exit(1)
-}
-
-func (s *SourceLocation) ThrowWarning(msg string, top bool, hints ...*Hint) {
-	if !top {
-		fmt.Fprintln(os.Stderr, "──────────────────────────────────────")
-	}
-
-	n, _ := fmt.Fprintf(os.Stderr, "[%s:%d:%d]", filepath.Base(s.File), s.Row, s.Column)
-	fmt.Fprintf(os.Stderr, " \x1b[33;1mWarning\x1b[0m: %s\n", msg)
 
 	code, ok := fileCache[s.File]
 
